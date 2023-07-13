a1451fb71c0c8ca031823ba87d278602a86a9570
Gunjan7991 <65468162+Gunjan7991@users.noreply.github.com>
2023-07-13 12:04:08 -0500

    Successfully fetched data from database.

--- app/routers/posts.py
+++ app/routers/posts.py
@@ -1,10 +1,15 @@
 from fastapi import APIRouter, HTTPException, status, Depends
 from ..database import get_db
 from sqlalchemy.orm import Session
+from ..models import posts
 
-router = APIRouter(prefix= "/api/v1",
-                   tags = ["Posts"])
+router = APIRouter(prefix="/api/v1",
+                   tags=["Posts"])
 
-@router.get("/posts")
+
+@router.get("/posting")
 async def get_post(db: Session = Depends(get_db)):
-    return {"message": "Success!!"}
+    my_posts = db.query(posts).all()
+    print(type(my_posts))
+    print(my_posts[0])
+    return {"message": "Success!!", "Posts": my_posts}
