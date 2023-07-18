344d2c84107a126cb7747507d30fb791a8cd9892
Gunjan7991 <65468162+Gunjan7991@users.noreply.github.com>
2023-07-18 12:28:23 -0500

    cleanup in main,  used router.

--- app/main.py
+++ app/main.py
@@ -1,30 +1,25 @@
-from fastapi import FastAPI, HTTPException, status, Depends
-from pydantic import BaseModel
-from typing import Optional
+from fastapi import FastAPI, APIRouter
 from . import models
-from .database import get_db, engine 
+from .database import engine 
 from .routers import posts
 
-models.Base.metadata.create_all(bind=engine)
-app = FastAPI()
 
+
+
+# Creates all the table in database based on the models.
+models.Base.metadata.create_all(bind=engine)
+
+
+#Starter Code for Fastapi
+app = FastAPI(title="FAST_API_PRAC",)
+@app.get("/")
+def status():
+    return{"message": "Server running..."}
+
+
+#include all the routes
 app.include_router(posts.router)
 
-class Post(BaseModel):
-    id: Optional[int] = None
-    title: str
-    content: str
-    published: bool = True
-    rating: Optional[int] = None
-    
-
-
-my_posts:list = [{"id": 1, "title": "Post1 ", "content": "Hello World", "published": True, "rating": 4}, {"id": 2, "title": "Post 2", "content": "My Name is slim shady.","published": True}, {"id": 3, "title": "Post 3", "content": "Hello from the other side!!", "published": False, "rating": 5}]
-
-
-@app.get("/")
-def root():
-    return {"message": "Server is running..."}
 
 
 
