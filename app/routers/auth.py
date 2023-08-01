7949f866c04c57f90b74d677ec89a5b40633f823
Gunjan7991 <65468162+Gunjan7991@users.noreply.github.com>
2023-08-01 18:01:39 -0500

    use of OAuthPasswordRequestForm, removal of schema.login, returns JWT token

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,17 +1,18 @@
 from fastapi import APIRouter, Depends, status, HTTPException
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from ..database import get_db
 from sqlalchemy.orm import Session
 from ..utils import verify_password, display_user
-from .. import schemas, models
+from .. import schemas, models, oauth
 
 router = APIRouter(prefix='/api/v1',
                    tags=['Authentication'])
 
 
 @router.post("/login", status_code=status.HTTP_202_ACCEPTED)
-def login(login_cred: schemas.login, db: Session = Depends(get_db)):
+def login(login_cred: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     user = db.query(models.users).filter(
-        models.users.email == login_cred.email).first()
+        models.users.email == login_cred.username).first()
     if not user:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Credentials")
@@ -20,7 +21,9 @@ def login(login_cred: schemas.login, db: Session = Depends(get_db)):
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Credentials")
 
+    access_token = oauth.create_access_token(
+        data={"user_id": user.id, "name": user.name})
     return {"message": "login sucessfull",
-            "Token": "Example Token",
+            "Token": {"access_token": access_token, "token_type": "bearer"},
             "Data": display_user(user)
             }
