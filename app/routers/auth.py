5e35ead719a2b2cc84a6f665386cf2ea75393382
Gunjan7991 <65468162+Gunjan7991@users.noreply.github.com>
2023-08-01 11:40:51 -0500

    Update to basic model for authentication
    updated to represent basic authentication. Still need to work on JWT config.

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,13 +1,26 @@
 from fastapi import APIRouter, Depends, status, HTTPException
 from ..database import get_db
 from sqlalchemy.orm import Session
-from ..utils import verify_password
-from .. import schemas
+from ..utils import verify_password, display_user
+from .. import schemas, models
 
 router = APIRouter(prefix='/api/v1',
                    tags=['Authentication'])
 
 
-router.post("/login", status_code=status.HTTP_202_ACCEPTED)
-def login(user: schemas.login, db:Session = Depends(get_db)):
-    pass
+@router.post("/login", status_code=status.HTTP_202_ACCEPTED)
+def login(login_cred: schemas.login, db: Session = Depends(get_db)):
+    user = db.query(models.users).filter(
+        models.users.email == login_cred.email).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Credentials")
+
+    if not verify_password(login_cred, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Credentials")
+
+    return {"message": "login sucessfull",
+            "Token": "Example Token",
+            "Data": display_user(user)
+            }
