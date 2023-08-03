a85c87c771e6839c74eea75bf416452b1136905c
Gunjan7991 <65468162+Gunjan7991@users.noreply.github.com>
2023-08-03 16:03:17 -0500

    status code updated to better convey the message

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -11,15 +11,16 @@ router = APIRouter(prefix='/api/v1',
 
 @router.post("/login", status_code=status.HTTP_202_ACCEPTED)
 def login(login_cred: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    user = db.query(models.users).filter(
+    user: models.users = db.query(models.users).filter(
         models.users.email == login_cred.username).first()
+
     if not user:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Credentials")
+            status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials")
 
     if not verify_password(login_cred.password, user.password):
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Credentials")
+            status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials")
 
     access_token = oauth.create_access_token(
         data={"user_id": user.id, "name": user.name})
