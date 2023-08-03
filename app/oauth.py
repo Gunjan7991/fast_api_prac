9d23459c2dad3459c2a89cf9103d137b3ec31de6
Gunjan7991 <65468162+Gunjan7991@users.noreply.github.com>
2023-08-02 20:24:56 -0500

    verify access token added

--- app/oauth.py
+++ app/oauth.py
@@ -1,6 +1,7 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
 from .config import settings
+from . import schemas
 
 
 def create_access_token(data: dict) -> str:
@@ -10,3 +11,21 @@ def create_access_token(data: dict) -> str:
     encoded = jwt.encode(to_encode, settings.secret_key,
                          algorithm=settings.algorithm)
     return encoded
+
+
+def verify_access_token(token: str, credentials_exceptions):
+    try:
+        payload = jwt.decode(token=token, key=settings.secret_key,
+                             algorithms=settings.algorithm)
+
+        id: str = payload.get("user_id")
+        name: str = payload.get("name")
+
+        if id is None:
+            raise credentials_exceptions
+
+        token_data = schemas.TokenData(id=id, name=name)
+
+    except JWTError:
+        raise credentials_exceptions
+    
