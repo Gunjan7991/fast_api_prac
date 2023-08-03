a4ed6e21da561c6bd212d5c146a94e3825fc4317
Gunjan7991 <65468162+Gunjan7991@users.noreply.github.com>
2023-08-03 17:07:24 -0500

    Added relationship/Foreign key
    Post has now foreign key associated with User table.

--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
 from .database import Base
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy.sql.expression import text
+from sqlalchemy.orm import relationship
 
 
 class posts(Base):
@@ -13,6 +14,9 @@ class posts(Base):
     published = Column(Boolean, default=False)
     created_at = Column(TIMESTAMP(timezone=True),
                         nullable=False, server_default=text('now()'))
+    owner_id = Column(Integer, ForeignKey(
+        "users.id", ondelete="Cascade"), nullable=False)
+    owner = relationship("users")
 
 
 class users(Base):
