0245d50d766e8bd2e646ad47f828eeefd0e8cd32
Gunjan7991 <65468162+Gunjan7991@users.noreply.github.com>
2023-08-07 16:04:09 -0500

    Updated with response schema model

--- app/routers/posts.py
+++ app/routers/posts.py
@@ -7,7 +7,7 @@ router = APIRouter(prefix="/api/v1",
                    tags=["Posts"])
 
 
-@router.get("/posts", status_code=status.HTTP_200_OK)
+@router.get("/posts", status_code=status.HTTP_200_OK, response_model=schemas.PostDisplay)
 async def get_post(db: Session = Depends(get_db)):
     try:
         my_posts = db.query(models.posts).all()
@@ -15,10 +15,10 @@ async def get_post(db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_417_EXPECTATION_FAILED,
                             detail=f"Exception Occured!! {e}")
     finally:
-        return {"message": "Success!!", "Posts": my_posts}
+        return my_posts
 
 
-@router.get("/posts/{id}", status_code=status.HTTP_200_OK)
+@router.get("/posts/{id}", status_code=status.HTTP_200_OK, response_model=schemas.PostDisplay)
 def get_post_by_id(id: int, db: Session = Depends(get_db)):
     try:
         my_posts = db.query(models.posts).filter(models.posts.id == id).first()
@@ -29,10 +29,10 @@ def get_post_by_id(id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=status.HTTP_417_EXPECTATION_FAILED,
                             detail=f"Exception Occured!! {e}")
     finally:
-        return {"message": "Success!!", "Post": my_posts}
+        return my_posts
 
 
-@router.post("/posts", status_code=status.HTTP_201_CREATED)
+@router.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.PostDisplay)
 def save_post(posts: schemas.PostCreate, db: Session = Depends(get_db), get_current_user: schemas.TokenData = Depends(oauth.get_current_user)):
 
     print(
@@ -48,10 +48,10 @@ def save_post(posts: schemas.PostCreate, db: Session = Depends(get_db), get_curr
                             detail=f"Exception Occured!! {e}")
 
     finally:
-        return {"message": posts}
+        return posts
 
 
-@router.put("/posts/{id}", status_code=status.HTTP_202_ACCEPTED)
+@router.put("/posts/{id}", status_code=status.HTTP_202_ACCEPTED, response_model=schemas.PostDisplay)
 def update_post(id: int, updated_post: schemas.PostUpdate, db: Session = Depends(get_db)):
     try:
         post_query = db.query(models.posts).filter(models.posts.id == id)
@@ -66,7 +66,7 @@ def update_post(id: int, updated_post: schemas.PostUpdate, db: Session = Depends
         raise HTTPException(status_code=status.HTTP_417_EXPECTATION_FAILED,
                             detail=f"Exception Occured!! {e}")
     finally:
-        return {"message": post_query.first()}
+        return post
 
 
 @router.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
