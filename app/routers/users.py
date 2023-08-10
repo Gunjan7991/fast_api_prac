c2da342bd81f3377acc1913a956f4bb4f582ed2d
Gunjan7991 <65468162+Gunjan7991@users.noreply.github.com>
2023-08-10 18:13:05 -0500

    Unnecessary crud operation removed. Added security
    CRUD operation like get_all_users and login have been removed because it poses a security concern.
    
    Added logic to secure CRUD operation get_user_by_id by verifying that only logged in user can access their and only their information.

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, HTTPException, status, Depends
 from ..database import get_db
 from sqlalchemy.orm import Session
-from .. import models, schemas
-from ..utils import hash, verify_password, display_user
+from .. import models, schemas, oauth
+from ..utils import hash, display_user
+
 
 router = APIRouter(prefix="/api/v1",
                    tags=["Users"])
@@ -20,11 +21,11 @@ def save_users(users: schemas.UserCreate, db: Session = Depends(get_db)):
 
     user_exits = db.query(models.users).filter(
         models.users.phone == users.phone).first()
-    
+
     if user_exits:
         raise HTTPException(
             status_code=status.HTTP_406_NOT_ACCEPTABLE, detail="Phone Number already Used.")
-    
+
     if users.password == users.re_password:
         new_user.password = hash(users.password)
         try:
@@ -45,7 +46,11 @@ def save_users(users: schemas.UserCreate, db: Session = Depends(get_db)):
 
 
 @router.get('/users/{id}', status_code=status.HTTP_200_OK)
-async def get_user(id: int, db: Session = Depends(get_db)):
+async def get_user(id: int, db: Session = Depends(get_db),  current_user: schemas.TokenData = Depends(oauth.get_current_user)):
+    if id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Access Denied!! Unauthorized to access other's information!")
+
     user = db.query(models.users).filter(models.users.id == id).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
@@ -53,31 +58,3 @@ async def get_user(id: int, db: Session = Depends(get_db)):
 
     usr = display_user(user)
     return usr
-
-
-@router.post('/users/login', status_code=status.HTTP_200_OK)
-async def get_user_by_email(login: schemas.login, db: Session = Depends(get_db)):
-    user = db.query(models.users).filter(
-        models.users.email == login.email).first()
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"User with Email does not exist!! Please Sign Up")
-    if not verify_password(login.password, user.password):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                            detail=f"User with Email or Password didn't match!")
-    usr = display_user(user)
-    return {"Message": "Login Successful", "Detail": usr}
-
-
-@router.get("/users")
-def get_users(db: Session = Depends(get_db)):
-    try:
-        users = db.query(models.users).all()
-        userList: list = []
-        for user in users:
-            userList.append(display_user(user))
-    except Exception as e:
-        raise HTTPException(status_code=status.HTTP_417_EXPECTATION_FAILED,
-                            detail=f"Exception Occured!! {e}")
-    finally:
-        return {"message": "Success!!", "Posts": userList}
