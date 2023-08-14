3720a5ecf826725f1a466f7421a84641f81b100b
Gunjan7991 <65468162+Gunjan7991@users.noreply.github.com>
2023-08-14 18:53:54 -0500

    minor change

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -2,12 +2,12 @@ from fastapi import APIRouter, Depends, status, HTTPException
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from ..database import get_db
 from sqlalchemy.orm import Session
-from ..utils import verify_password, display_user
+from ..utils import verify_password
 from .. import schemas, models, oauth
+
 
 router = APIRouter(prefix='/api/v1',
                    tags=['Authentication'])
-
 
 @router.post("/login", status_code=status.HTTP_202_ACCEPTED, response_model=schemas.Token)
 def login(login_cred: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
