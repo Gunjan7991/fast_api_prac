8063ac0c5f301fe1e9d634c530e05eba95669df4
Gunjan7991 <65468162+Gunjan7991@users.noreply.github.com>
2023-08-15 01:24:28 -0500

    black formatting, adding limiter

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,39 +1,54 @@
-from fastapi import APIRouter, HTTPException, status, Depends
+from fastapi import APIRouter, HTTPException, status, Depends, Request
 import logging
 import re
+from slowapi import Limiter
+from slowapi.util import get_remote_address
+
 from ..database import get_db
 from sqlalchemy.orm import Session
 from .. import models, schemas, oauth
 from ..utils import hash, display_user, verify_password
 
 
-router = APIRouter(prefix="/api/v1",
-                   tags=["Users"])
-
+router = APIRouter(prefix="/api/v1", tags=["Users"])
 logger = logging.getLogger(__name__)
+limiter = Limiter(key_func=get_remote_address)
 
 
 @router.post("/users", status_code=status.HTTP_201_CREATED)
-def save_users(users: schemas.UserCreate, db: Session = Depends(get_db)):
-    new_user: models.users = models.users(
-        **users.model_dump(exclude="re_password"))
-    user_exits = db.query(models.users).filter(
-        models.users.email == users.email).first()
+@limiter.limit("1/minute")
+def save_users(
+    request: Request, users: schemas.UserCreate, db: Session = Depends(get_db)
+):
+    new_user: models.users = models.users(**users.model_dump(exclude="re_password"))
+    user_exits = (
+        db.query(models.users).filter(models.users.email == users.email).first()
+    )
     if user_exits:
         raise HTTPException(
-            status_code=status.HTTP_406_NOT_ACCEPTABLE, detail="Email already Used.")
+            status_code=status.HTTP_406_NOT_ACCEPTABLE, detail="Email already Used."
+        )
 
-    user_exits = db.query(models.users).filter(
-        models.users.phone == users.phone).first()
+    user_exits = (
+        db.query(models.users).filter(models.users.phone == users.phone).first()
+    )
 
     if user_exits:
         raise HTTPException(
-            status_code=status.HTTP_406_NOT_ACCEPTABLE, detail="Phone Number already Used.")
+            status_code=status.HTTP_406_NOT_ACCEPTABLE,
+            detail="Phone Number already Used.",
+        )
 
     if users.password == users.re_password:
-        if not re.match(r"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$", users.password):
-            raise HTTPException(status_code=status.HTTP_501_NOT_IMPLEMENTED, detail="Secure your password")
-        
+        if not re.match(
+            r"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$",
+            users.password,
+        ):
+            raise HTTPException(
+                status_code=status.HTTP_501_NOT_IMPLEMENTED,
+                detail="Secure your password",
+            )
+
         new_user.password = hash(users.password)
         try:
             db.add(new_user)
@@ -41,50 +56,73 @@ def save_users(users: schemas.UserCreate, db: Session = Depends(get_db)):
             db.refresh(new_user)
 
         except Exception as e:
-            raise HTTPException(status_code=status.HTTP_417_EXPECTATION_FAILED,
-                                detail=f"Exception Occured!! {e}")
+            raise HTTPException(
+                status_code=status.HTTP_417_EXPECTATION_FAILED,
+                detail=f"Exception Occured!! {e}",
+            )
 
         finally:
             return {"message": "Success!", "Data": display_user(new_user)}
 
     else:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                            detail=f"Password didn't Match")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail=f"Password didn't Match"
+        )
 
 
-@router.get('/users/{id}', status_code=status.HTTP_200_OK)
-def get_user(id: int, db: Session = Depends(get_db),  current_user: schemas.TokenData = Depends(oauth.get_current_user)):
+@router.get("/users/{id}", status_code=status.HTTP_200_OK)
+@limiter.limit("5/minute")
+def get_user(
+    request: Request,
+    id: int,
+    db: Session = Depends(get_db),
+    current_user: schemas.TokenData = Depends(oauth.get_current_user),
+):
     if id != current_user.id:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                            detail="Access Denied!! Unauthorized to access other's information!")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Access Denied!! Unauthorized to access other's information!",
+        )
 
     user = db.query(models.users).filter(models.users.id == id).first()
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"User with id: {id} does not exist")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"User with id: {id} does not exist",
+        )
 
     usr = display_user(user)
     return usr
 
 
 @router.put("/users/update/{id}", status_code=status.HTTP_202_ACCEPTED)
-def update_user(id: int, updated_user: schemas.UserUpdate, db: Session = Depends(get_db), current_user: schemas.TokenData = Depends(oauth.get_current_user)):
+@limiter.limit("5/minute")
+def update_user(
+    request: Request,
+    id: int,
+    updated_user: schemas.UserUpdate,
+    db: Session = Depends(get_db),
+    current_user: schemas.TokenData = Depends(oauth.get_current_user),
+):
     if id != current_user.id:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                            detail="Access Denied!! Unauthorized to access other's information!")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Access Denied!! Unauthorized to access other's information!",
+        )
     user_info = db.query(models.users).filter(models.users.id == id)
     user = user_info.first()
     update: bool = False
     if not verify_password(updated_user.old_password, user.password):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
-                            detail="Authentication Failed")
-    
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Authentication Failed"
+        )
+
     if not updated_user.address:
         logger.info("update function: address not passed")
         updated_user.address = user.address
     else:
         update = True
-        
+
     if not updated_user.phone:
         logger.info("update function: phone not passed")
         updated_user.phone = user.phone
@@ -100,13 +138,15 @@ def update_user(id: int, updated_user: schemas.UserUpdate, db: Session = Depends
             update = True
         else:
             raise HTTPException(
-                status_code=status.HTTP_304_NOT_MODIFIED, detail="Password didn't match")
+                status_code=status.HTTP_304_NOT_MODIFIED, detail="Password didn't match"
+            )
     message = {}
     if update:
         update = user_info.update(
-            updated_user.model_dump(exclude=["re_password", "old_password"]), synchronize_session=False)
-        logger.info(
-            f"Update Status: {'Updated' if update == 1 else 'Not Updated'}")
+            updated_user.model_dump(exclude=["re_password", "old_password"]),
+            synchronize_session=False,
+        )
+        logger.info(f"Update Status: {'Updated' if update == 1 else 'Not Updated'}")
         db.commit()
         user = db.query(models.users).filter(models.users.id == id).first()
         message = {"Message": "Updated", "User_Info": display_user(user)}
