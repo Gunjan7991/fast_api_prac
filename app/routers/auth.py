4e85bea0e8fb904e591d6d51b27ff53d65df4397
Gunjan7991 <65468162+Gunjan7991@users.noreply.github.com>
2023-08-15 01:22:37 -0500

    black formatting,  adding limiter.

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,28 +1,45 @@
-from fastapi import APIRouter, Depends, status, HTTPException
+from fastapi import APIRouter, Depends, Request, status, HTTPException
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
+from slowapi import Limiter
+from slowapi.util import get_remote_address
+import logging
 from ..database import get_db
 from sqlalchemy.orm import Session
 from ..utils import verify_password
 from .. import schemas, models, oauth
 
 
-router = APIRouter(prefix='/api/v1',
-                   tags=['Authentication'])
+router = APIRouter(prefix="/api/v1", tags=["Authentication"])
+logger = logging.getLogger(__name__)
+limiter = Limiter(key_func=get_remote_address)
 
-@router.post("/login", status_code=status.HTTP_202_ACCEPTED, response_model=schemas.Token)
-def login(login_cred: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    user: models.users = db.query(models.users).filter(
-        models.users.email == login_cred.username).first()
+
+@router.post(
+    "/login", status_code=status.HTTP_202_ACCEPTED, response_model=schemas.Token
+)
+@limiter.limit("1/minute")
+def login(
+    request: Request,
+    login_cred: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db),
+):
+    logger.info(f"Request: {request.__dict__['scope']['client']}")
+    user: models.users = (
+        db.query(models.users).filter(models.users.email == login_cred.username).first()
+    )
 
     if not user:
         raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials")
+            status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials"
+        )
 
     if not verify_password(login_cred.password, user.password):
         raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials")
+            status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials"
+        )
 
     access_token = oauth.create_access_token(
-        data={"user_id": user.id, "name": user.name})
+        data={"user_id": user.id, "name": user.name}
+    )
 
     return schemas.Token(access_token=access_token, token_type="Bearer")
