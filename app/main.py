8f38e6a64e74872124f165b7ec30f2ab3f9449b4
Gunjan7991 <65468162+Gunjan7991@users.noreply.github.com>
2023-08-15 01:25:17 -0500

    black formatting, adding limiter

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
 from . import models, setting
 from .database import engine
 from .routers import posts, users, auth, likes, comments
@@ -6,17 +9,23 @@ from .routers import posts, users, auth, likes, comments
 # Creates all the table in database based on the models.
 models.Base.metadata.create_all(bind=engine)
 
-#importing logger setup
+# importing logger setup
 logger = setting.logger()
 
 # Starter Code for Fastapi
-app = FastAPI(title=setting.details["title"], summary=setting.details["summary"], description=setting.details["description"], version="1.0", openapi_tags=setting.tags_metadata)
-
+limiter = Limiter(key_func=get_remote_address)
+app = FastAPI(
+    title=setting.details["title"],
+    summary=setting.details["summary"],
+    description=setting.details["description"],
+    version="1.0",
+    openapi_tags=setting.tags_metadata,
+)
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 # include all the routes
 app.include_router(posts.router)
 app.include_router(users.router)
 app.include_router(likes.router)
 app.include_router(comments.router)
 app.include_router(auth.router)
-
-
